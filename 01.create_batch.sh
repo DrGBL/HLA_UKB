0c951b455e91d557e97afdc0786e64bc2fb9e46e
DrGBL <guillaume_bl@hotmail.com>
2022-01-31 10:41:10 -0500

    Update 01.create_batch.sh

--- 01.create_batch.sh
+++ 01.create_batch.sh
@@ -1,6 +1,13 @@
+#the goal of this code was to create batches to submit to dna nexus (the "batches/batch_${folder}.tsv" files at the end). 
+#Column names are important and very tricky, see more here: https://documentation.dnanexus.com/user/running-apps-and-workflows/running-batch-jobs
+
+#I created a conda environment with the dna-nexus python package: https://documentation.dnanexus.com/downloads
 conda activate dnanexus-env
 
-# dx login
+dx login #enter your username and password
+
+#below I'm showing what I did for the HLA work, adapt as needed
+#it assumes that there's a folder called "batches" and another one named "batches_prelim" in the working directory
 
 cd /project/richards/guillaume.butler-laporte/HLA/ukb_wes/
 
@@ -9,41 +16,14 @@ dx cd '/Bulk/Exome sequences/Exome OQFE CRAM files/'
 dx ls > folders_full_exomes.txt
 sed -i 's/\///' folders_full_exomes.txt
 
-# File cram_file
-# File ref_genome
-# File ref_genome_index
-# File ref_genome_dict
-# File ref_genome_gzi
-# Int cores_hla
-# Int min_read_length
-
-#rm batches_prelim/*
-
 while read folder; do
-  dx generate_batch_inputs -icram_file='(.*).cram$' --path 'exome_full:Bulk/Exome sequences/Exome OQFE CRAM files/'${folder}'/' -o 'batches_prelim_tianyuan/folder_'${folder}
+  dx generate_batch_inputs -icram_file='(.*).cram$' --path 'exome_full:Bulk/Exome sequences/Exome OQFE CRAM files/'${folder}'/' -o 'batches_prelim/folder_'${folder}
 done < folders_full_exomes.txt
 
-folder=30
-head -n 1 batches_prelim/folder_${folder}.0000.tsv > batches/batch_${folder}.tsv
-awk 'FNR>1' batches_prelim/folder_${folder}* >> batches/batch_${folder}.tsv
-sed -i 's/cram_file/stage-common.cram_file/g' batches/batch_${folder}.tsv
-
-
+read folder; do
+  head -n 1 batches_prelim/folder_${folder}.0000.tsv > batches/batch_${folder}.tsv
+  awk 'FNR>1' batches_prelim/folder_${folder}* >> batches/batch_${folder}.tsv
+  sed -i 's/cram_file/stage-common.cram_file/g' batches/batch_${folder}.tsv
+done < folders_full_exomes.txt
 
 dx cd /
-
-#rm batches_prelim/*.tsv
-
-
-
-#for tianyuan
-folder=60
-head -n 1 batches_prelim_tianyuan/folder_${folder}.0000.tsv > batches_tianyuan/batch_${folder}.tsv
-awk 'FNR>1' batches_prelim_tianyuan/folder_${folder}* >> batches_tianyuan/batch_${folder}.tsv
-sed -i 's/cram_file/stage-common.cram_file/g' batches_tianyuan/batch_${folder}.tsv
-
-
-#for yiheng
-head -n 1 batches_prelim_yiheng/folder_43.0000.tsv > batches_yiheng/batch_43.tsv
-awk 'FNR>1' batches_prelim_yiheng/folder_43* >> batches_yiheng/batch_43.tsv
-sed -i 's/cram_file/stage-common.cram_file/g' batches_yiheng/batch_43.tsv
