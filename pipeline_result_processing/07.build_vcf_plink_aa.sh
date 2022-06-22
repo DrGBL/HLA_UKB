95834b04a9c73d700b7afaf17919c5e61bce823c
DrGBL <guillaume_bl@hotmail.com>
2022-06-22 15:40:04 -0400

    Update 07.build_vcf_plink_aa.sh

--- pipeline_result_processing/07.build_vcf_plink_aa.sh
+++ pipeline_result_processing/07.build_vcf_plink_aa.sh
@@ -1,6 +1,9 @@
 #move to the "amino_acids" directory. 
 cd /path/to/amino_acids/
-  
+
+#vcf header to insert
+printf "##fileformat=VCFv4.3\n##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">" | gzip > vcf_header.txt.gz
+
 declare -a genes_hla=("A" "B" "C" "E" "F" "G" "DMA" "DMB" "DOA" "DOB" "DPA1" "DPB1" "DQA1" "DQB1" "DRA" "DRB1" "DRB3" "DRB4" "DRB5")
   
 for g in "${genes_hla[@]}"; do
