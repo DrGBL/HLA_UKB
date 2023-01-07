245bee18a627333f28cba016ad23b9e7167a4562
Guillaume Butler-Laporte <guillaume_bl@hotmail.com>
2023-01-07 21:15:02 +0000

    Update and rename 01.tmp.R to 01.regular_LD_calculation.R

---
+++ step.05.misc/01.regular_LD_calculation.R
@@ -0,0 +1,28 @@
+pathPlink=/project/richards/guillaume.butler-laporte/HLA/ukb_wes/vcf/
+pathOut=/path/to/ld_results/
+pathAnc=/path/to/ancestry/files/    #these are files containing FID and IID of each participants in each genetic ancestry
+
+declare -a anc=("afr" "amr" "eas" "eur" "sas")
+
+for a in "${anc[@]}"
+do
+  plink \
+    --r2 square gz \
+    --bfile ${pathPlink}full_hla_four_digit \
+    --keep ${pathAnc}ukb.${a}IDsPCA.plink_final.txt \
+    --out ${pathOut}ld_four_digit_${a}
+  
+  plink \
+    --r2 square gz \
+    --bfile ${pathPlink}full_hla_six_digit \
+    --keep ${pathAnc}ukb.${a}IDsPCA.plink_final.txt \
+    --out ${pathOut}ld_six_digit_${a}
+    
+  plink \
+    --r2 square gz \
+    --bfile ${pathPlink}full_hla_two_digit \
+    --keep ${pathAnc}ukb.${a}IDsPCA.plink_final.txt \
+    --out ${pathOut}ld_two_digit_${a}
+done
+
+
